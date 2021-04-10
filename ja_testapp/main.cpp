f6d603e315923f53123ac4606452ceeefbc1f10b
rileyzzz <rileylemmler@gmail.com>
2021-04-10 02:44:25 +0000

    stuff

--- ja_testapp/main.cpp
+++ ja_testapp/main.cpp
@@ -15,12 +15,14 @@ int main(int argc, char* argv[])
     //char* Buffer1;
     char* Buffer2;
     int Count2 = 0;
-    char* Temp1 = new char[256];
-    char* Temp2 = new char[256];
-    char* Temp3 = new char[256];
-    char* Temp4 = new char[256];
+    unsigned char* Keys = new unsigned char[256];
+    unsigned char* Temp2 = new unsigned char[256];
+    unsigned char* Values = new unsigned char[256];
+
+    //dictionary between temp1 and temp4
+
     unsigned char TempChar = 0;
-    int Counter = 0, Temp = 0, Temp2value = 0;
+    int Counter = 0, Head = 0, Temp2value = 0;
     short BlockSize = 0;
     
     int TempIt = 0;
@@ -40,34 +42,38 @@ int main(int argc, char* argv[])
     //while(!str.eof())
     {
         for(int i = 0; i < 256; i++)
-            Temp1[i] = Temp3[i] = i;
+            Keys[i] = i;
         
-        READ(str, TempChar);
-        Temp = 0;
+        //READ(str, TempChar);
+        Head = 0;
         printf("initial char %d\n", TempChar);
 
-        while(Temp < 256)
+        while(Head < 256)
         {
+            READ(str, TempChar);
             if(TempChar > 0x7F)
             {
-                Temp += TempChar - 0x7F;
-                printf("temp increment, add %d value %d\n", TempChar, Temp);
+                printf("temp increment, add value %d to %d\n", TempChar - 0x7F, Head);
+                Head += TempChar - 0x7F;
             }
             else
             {
-                printf("temp value %d, char %d\n", Temp, TempChar);
+                printf("temp value %d, char %d, begin loop\n", Head, TempChar);
                 for(int i = 0; i < TempChar + 1; i++)
                 {
-                    READ(str, Temp1[Temp]);
-                    if(Temp != Temp1[Temp])
-                        READ(str, Temp4[Temp]);
+                    READ(str, Keys[Head]);
+                    printf("read character %d to temp buffer 1, Temp is %d\n", Keys[Head], Head);
+
+                    //if the character isn't the end - key character != current offset position
+                    if(Head != Keys[Head])
+                        READ(str, Values[Head]);
                     
-                    Temp++;
+                    Head++;
                 }
             }
 
-            if(Temp < 256)
-                READ(str, TempChar);
+            //if(Temp < 256)
+                //READ(str, TempChar);
         }
 
         //read next two characters, swap
@@ -90,38 +96,40 @@ int main(int argc, char* argv[])
             if(Temp2value != 0)
             {
                 Temp2value--;
-                Temp = Temp2[Temp2value];
+                Head = Temp2[Temp2value];
             }
             else
             {
                 printf("temp2val 0! TempIt %d\n", TempIt);
-                Temp = TempIt;
+                Head = TempIt;
                 TempIt--;
-                if(Temp == 0)
+                if(Head == 0)
                     break;
 
                 char t = 0;
                 READ(str, t);
-                Temp = t;
+                Head = t;
                 //READ(str, Temp);
             }
 
-            TempChar = Temp1[Temp];
-            if(Temp == (TempChar & 0xFF))
+            TempChar = Keys[Head];
+            //literal!!!!
+            if(Head == (TempChar & 0xFF))
             {
+                printf("literal %d, tempchar %d\n", Head, TempChar);
                 //hacky prevent segfault
                 //if(Count2 > 1000) break;
 
                 //write to final
                 //printf("write character %c\n", Temp);
-                Buffer2[Count2] = Temp;
+                Buffer2[Count2] = Head;
                 Count2++;
             }
-            else
+            else //reference!!!
             {
-                printf("temp2\n");
-                Temp = Temp4[Temp];
-                Temp2[Temp2value] = Temp;
+                printf("reference\n");
+                Head = Values[Head];
+                Temp2[Temp2value] = Head;
                 Temp2[Temp2value + 1] = TempChar;
                 Temp2value += 2;
             }
